989ad1abae64e4b96169bc76eaf4b0be173d2e9b
Touhidujjaman-Emon <141167402+Touhidujjaman-Emon@users.noreply.github.com>
2023-11-21 17:34:29 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      xsm:'371px',
+      sm: '480px',
+      sm2: '668px',
+      md: '768px',
+      lg: '976px',
+      lg2: '1200px',
+      xl: '1344px',
+    },
+    extend: {
+      colors: {
+        primary: '#e67e22',
+        tint: '#fdf2e9',
+        tint2: '#fae5d3',
+        shade: '#cf711f',
+        shade2: '#45260a',
+        
+      },
+      fontSize : {
+
+        font1:'0.625rem',
+        font2:'0.75rem',
+        font3:'0.875rem',
+        font4:'1rem',
+        font5:'1.125rem',
+        font6:'1.25rem',
+        font7:'1.5rem',
+        font8:'1.875rem',
+        font9:'2.25rem',
+        font10:'2.75rem',
+        font11:'3.25rem',
+        font12:'3.875rem',
+        font13:'4.625rem',
+        font14:'5.375rem',
+        font15:'6rem',
+        
+
+      },
+      fontFamily: {
+       rubik : ['Rubik', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
